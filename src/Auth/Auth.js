ff098034bb3bb5fed78d16cbec37bcf50e26d79e
montanafish <mitch@officesato.com>
2018-10-30 22:56:18 +1100

    bypassed auth0

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,64 +1,65 @@
-import auth0 from 'auth0-js';
-import history from '../history';
+import auth0 from 'auth0-js'
+import history from '../history'
 
 export default class Auth {
-  auth0 = new auth0.WebAuth({
-    domain: 'hasuratodo.auth0.com',
-    clientID: process.env.REACT_APP_CLIENTID,
-    redirectUri: process.env.REACT_APP_REDIRECTURI,
-    responseType: 'token id_token',
-    scope: 'openid'
-  });
+  auth0 = {}
 
   constructor() {
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+    this.handleAuthentication = this.handleAuthentication.bind(this)
+    this.isAuthenticated = this.isAuthenticated.bind(this)
   }
 
   login() {
-    this.auth0.authorize();
+    //this.auth0.authorize();
+    // Set the time that the Access Token will expire at
+    let expiresAt = JSON.stringify(new Date() * 1000 + new Date().getTime())
+    localStorage.setItem('access_token', '3ktV0c%@htCZ*X3')
+    //localStorage.setItem('id_token', authResult.idToken)
+    localStorage.setItem('expires_at', expiresAt)
+    //localStorage.setItem('sub', authResult.idTokenPayload.sub)
+    // navigate to the home route
+    history.replace('/manage')
   }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-        history.replace('/manage');
+        this.setSession(authResult)
+        history.replace('/manage')
       } else if (err) {
-        history.replace('/manage');
-        console.log(err);
+        history.replace('/manage')
+        console.log(err)
       }
-    });
+    })
   }
 
   setSession(authResult) {
     // Set the time that the Access Token will expire at
-    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
-    localStorage.setItem('sub', authResult.idTokenPayload.sub);
+    let expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime())
+    localStorage.setItem('access_token', authResult.accessToken)
+    localStorage.setItem('id_token', authResult.idToken)
+    localStorage.setItem('expires_at', expiresAt)
+    localStorage.setItem('sub', authResult.idTokenPayload.sub)
     // navigate to the home route
-    history.replace('/manage');
+    history.replace('/manage')
   }
 
   logout() {
     // Clear Access Token and ID Token from local storage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    localStorage.removeItem('sub');
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('id_token')
+    localStorage.removeItem('expires_at')
+    localStorage.removeItem('sub')
     // navigate to the home route
-    history.replace('/manage');
+    history.replace('/manage')
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
+    // Check whether the current time is past the
     // Access Token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'))
+    return new Date().getTime() < expiresAt
   }
-
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import GetTodos from './GetTodos';
+import React, { Component } from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import GetTodos from './GetTodos'
 
-export var client;
+export var client
 
 class Home extends Component {
   constructor(props) {
-    super(props);
-    const ACCESS_TOKEN = localStorage.getItem('access_token');
+    super(props)
+    const ACCESS_TOKEN = '3ktV0c%@htCZ*X3'
     client = new ApolloClient({
-      uri: "https://hasura-react-todo.herokuapp.com/v1alpha1/graphql",
+      uri: 'https://hamlet-staging.herokuapp.com/v1alpha1/graphql',
       headers: {
-        'Authorization': `Bearer ${ACCESS_TOKEN}`,
-      }
-    });
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    })
   }
 
   login() {
-    this.props.auth.login();
+    this.props.auth.login()
   }
   render() {
-    const { isAuthenticated } = this.props.auth;
+    const { isAuthenticated } = this.props.auth
     return (
       <div className="container">
-        {
-          isAuthenticated() && (
-            <ApolloProvider client={client}>
-              <GetTodos />
-            </ApolloProvider>
-          )
-        }
+        {isAuthenticated() && (
+          <ApolloProvider client={client}>
+            <GetTodos />
+          </ApolloProvider>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
